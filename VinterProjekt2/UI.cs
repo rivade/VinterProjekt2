3f57273051e3144f3267583857cba9a7b60d258f
theo lindqvist <theo.lindqvist@elev.ga.ntig.se>
2023-11-23 14:44:23 +0100

    progress2

--- VinterProjekt2/UI.cs
+++ VinterProjekt2/UI.cs
@@ -1,13 +1,30 @@
+using Raylib_cs;
+
 public class UIscreen
 {
-    public virtual void Draw() {}
+    protected Texture2D background;
+    public virtual void Draw()
+    {
+        Raylib.DrawTexture(background, 0, 0, Color.WHITE);
+    }
 }
 
 public class StartScreen : UIscreen
-{}
+{
+    public StartScreen()
+    {
+        
+    }
+}
 
 public class GameOverScreen : UIscreen
-{}
+{
+    public GameOverScreen()
+    {}
+}
 
 public class WinScreen : UIscreen
-{}
+{
+    public WinScreen()
+    {}
+}
