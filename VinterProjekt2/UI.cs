47322c9282142f8529bc3423ff251214426c0f04
theo lindqvist <theo.lindqvist@elev.ga.ntig.se>
2023-12-06 21:07:13 +0100

    Startscreen bg scrolling + center text method

--- VinterProjekt2/UI.cs
+++ VinterProjekt2/UI.cs
@@ -1,13 +1,18 @@
 using Raylib_cs;
 using System.Numerics;
 using System.Linq;
+using System.Text.RegularExpressions;
+using System.Runtime.InteropServices;
 
 public class UIscreen
 {
+    const int lineHeight = 40;
+
     protected Player player;
     protected Texture2D background;
     protected Rectangle button;
     protected Color buttonColor;
+    protected string[] textMessages;
 
 
     public virtual void Logic(Level level)
@@ -33,24 +38,48 @@ public class UIscreen
         Raylib.DrawTexture(background, 0, 0, Color.WHITE);
         Raylib.DrawRectangleRec(button, buttonColor);
     }
+
+    protected void DrawCenteredText(int startY, Color textcolor)
+    {
+        for (int i = 0; i < textMessages.Length; i++)
+        {
+            int textWidth = Raylib.MeasureText(textMessages[i], 40);
+            int startX = (GameManager.screenWidth - textWidth) / 2;
+
+            Raylib.DrawText(textMessages[i], startX, startY + i * lineHeight, 40, textcolor);
+        }
+    }
 }
 
 public class StartScreen : UIscreen
 {
+    Rectangle sourceRec;
+    Texture2D titleText = Raylib.LoadTexture("Sprites/titletext.png");
+
     public StartScreen(Player inPlayer)
     {
         player = inPlayer;
         background = Raylib.LoadTexture("Backgrounds/startscreen.png");
         button = new((GameManager.screenWidth / 2) - 110, (GameManager.screenHeight / 2) + 40, 220, 95);
+        sourceRec = new Rectangle(0, 0, GameManager.screenWidth, GameManager.screenHeight);
+
+        textMessages = new string[]
+        {
+            "Press I for instructions",
+            "Press L for level selection"
+        };
     }
 
     public override void Draw()
     {
-        base.Draw();
-        Raylib.DrawText("Jumpman 2", 300, 200, 75, Color.WHITE);
-        Raylib.DrawText("START", 433, 445, 40, Color.BLACK);
-        Raylib.DrawText("Press I for instructions!", 228, 550, 40, Color.RED);
-        Raylib.DrawText("Press L for level select!", 228, 600, 40, Color.RED);
+        sourceRec.x += 0.5f; //Sourcerektangelns x-värde motsvarar offseten i bakgrunds skrollningen.
+        Raylib.BeginDrawing();
+        Raylib.ClearBackground(Color.WHITE);
+        Raylib.DrawTextureRec(background, sourceRec, new Vector2(0, 0), Color.WHITE);
+        Raylib.DrawRectangleRec(button, buttonColor);
+        Raylib.DrawTexture(titleText, GameManager.screenWidth / 2 - titleText.width / 2, 200, Color.WHITE);
+        Raylib.DrawText("START", (int)button.x + 23, (int)button.y + 25, 50, Color.BLACK);
+        DrawCenteredText(550, Color.RED);
         Raylib.EndDrawing();
     }
 
@@ -115,34 +144,27 @@ public class WinScreen : UIscreen
 
 public class InfoScreen : UIscreen
 {
-    private string[] textMessages = {
-        "Use A/D or left/right arrow keys to move",
-        "Press SPACE or Up arrow key to jump",
-        "You can jump when moving against walls,",
-        "but you have to keep pushing into them!",
-        "Avoid the spikes as they kill you!",
-        "Get to the portal at the end to win.",
-        "Press M at any time to return to menu!",
-    };
-    private readonly int lineHeight = 40;
-
     public InfoScreen(Player inPlayer)
     {
         player = inPlayer;
         button = new((GameManager.screenWidth / 2) - 110, (GameManager.screenHeight / 2) + 140, 220, 95);
+        textMessages = new string[]
+        {
+            "Use A/D or left/right arrow keys to move",
+            "Press SPACE or Up arrow key to jump",
+            "You can jump when moving against walls,",
+            "but you have to keep pushing into them!",
+            "Avoid the spikes as they kill you!",
+            "Get to the portal at the end to win.",
+            "Press M at any time to return to menu!",
+        };
     }
 
     public override void Draw()
     {
         Raylib.BeginDrawing();
         Raylib.ClearBackground(Color.BLUE);
-        for (int i = 0; i < textMessages.Length; i++)
-        {
-            int textWidth = Raylib.MeasureText(textMessages[i], 40);
-            int startX = (GameManager.screenWidth - textWidth) / 2;
-
-            Raylib.DrawText(textMessages[i], startX, 100 + i * lineHeight, 40, Color.BLACK);
-        }
+        DrawCenteredText(200, Color.BLACK);
         Raylib.DrawRectangleRec(button, buttonColor);
         Raylib.DrawText("START", 433, 545, 40, Color.BLACK);
         Raylib.EndDrawing();
